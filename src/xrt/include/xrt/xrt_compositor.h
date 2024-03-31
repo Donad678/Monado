12e009e358c4c31c94c559ee5586366b77646f22
korejan <64199710+korejan@users.noreply.github.com>
2024-03-31 15:52:14 +0100

    xrt: Adds support for XR_META_body_tracking_calibration

--- src/xrt/include/xrt/xrt_compositor.h
+++ src/xrt/include/xrt/xrt_compositor.h
@@ -974,6 +974,7 @@ struct xrt_begin_session_info
 	bool fb_face_tracking2_enabled;
 	bool meta_body_tracking_full_body_enabled;
 	bool meta_body_tracking_fidelity_enabled;
+	bool meta_body_tracking_calibration_enabled;
 };
 
 /*!
